e24729dbd5bc90f2894d38814e86a8bb014ff952
samulinukala <samulinukala740@gmail.com>
2026-04-29 16:56:47 +0300

    added route to backend

--- server.js
+++ server.js
@@ -261,7 +261,7 @@ else (res.status(500).json({"account creation":"failed"}))
 
 app.get('/api/users/login/:un/:pw',(req,res)=>
 {
-res.set('Access-Control-Allow-Origin','*');
+res.set('Access-Control-Allow-Origin','https://samulinukala.github.io');
 res.set("Access-Control-Allow-Credentials: true")
 res.set('Access-Control-Allow-Methods','GET,POST,PUT');
 console.log("try");
